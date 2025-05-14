54157d3a6321ad3d715d5a1554b2eb0f893dd9ae
Topaz24 <topaz2490@gmail.com>
2025-05-15 00:14:40 +0300

    added user data

---
+++ user_data/user_data_app.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Add Docker's official GPG key:
+apt-get update -y
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+usermod -aG docker ubuntu
+docker run -d --name myapp -e DB_NAME=ToDoDB -e DB_USER=todo_user -e DB_PASSWORD=1234 -e DB_HOST=172.31.30.100 -p 5000:5000 toph2490/my_to_do_list
+
+
+
+
---
+++ user_data/user_data_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Add Docker's official GPG key:
+apt-get update -y
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+   tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run --restart unless-stopped -d -e MYSQL_ROOT_PASSWORD=rootpassword -e MYSQL_DATABASE=ToDoDB -e MYSQL_USER=todo_user -e MYSQL_PASSWORD=1234 -v sql_data:/var/lib/mysql -p 3306:3306 mysql:8.0
+
